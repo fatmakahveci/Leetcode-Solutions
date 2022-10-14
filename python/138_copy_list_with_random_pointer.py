4ca39c84ad9105818e1eefc2bedc17286afdb17c
Fatma Kahveci <fatmaba@gmail.com>
2022-10-14 09:29:27 +0300

    Create 138_copy_list_with_random_pointer.py

---
+++ python/138_copy_list_with_random_pointer.py
@@ -0,0 +1,27 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def __init__(self):
+        self.visited_map = {}
+
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head:
+            return None
+        
+        if head in self.visited_map:
+            return self.visited_map[head]
+        
+        node = Node(head.val, None, None)
+        self.visited_map[head] = node
+        node.next = self.copyRandomList(head.next)
+        node.random = self.copyRandomList(head.random)
+        
+        return node
+    
