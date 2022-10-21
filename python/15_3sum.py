892462c8952c230b106f91d0b0842f7191962a30
Fatma Kahveci <fatmaba@gmail.com>
2022-10-21 10:53:05 +0100

    Update 15_3sum.py

--- python/15_3sum.py
+++ python/15_3sum.py
@@ -1,25 +1,33 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
+
+        if len(nums) < 3:
+            return []
+    
         nums.sort()
+        
+        if nums[0] > 0 or nums[-1] < 0:
+            return []
 
+        res = []  
         for i in range(len(nums)-2):
-            if i == 0 or (i > 0 and nums[i] != nums[i-1]):
-                low = i+1
-                high = len(nums)-1
-                third_num = -nums[i]
-                while low < high:
-                    if nums[low] + nums[high] == third_num:
-                        res.append([nums[low], nums[high], -third_num])
-                        while low < high and nums[low] == nums[low+1]:
-                            low += 1
-                        while low < high and nums[high] == nums[high-1]:
-                            high -= 1
-                        low += 1
-                        high -= 1
-                    elif nums[low] + nums[high] < third_num:
-                        low += 1
-                    elif nums[low] + nums[high] > third_num:
-                        high -= 1
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
 
+            lo, hi = i+1, len(nums)-1
+            target = -nums[i]
+            while lo < hi:
+                if nums[lo] + nums[hi] < target:
+                    lo += 1
+                elif nums[lo] + nums[hi] > target:
+                    hi -= 1
+                else:
+                    res.append([ nums[lo], nums[i], nums[hi] ])
+                    while lo < hi and nums[hi-1] == nums[hi]:
+                        hi -= 1
+                    while lo < hi and nums[lo] == nums[lo+1]:
+                        lo += 1
+                    lo += 1
+                    hi -= 1
+        
         return res
