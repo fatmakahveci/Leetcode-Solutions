203eefeddac8fe7e274976dec8dca5c60a3d3b0f
Fatma Kahveci <fatmaba@gmail.com>
2022-11-24 21:58:27 +0000

    Update 1587_bank_account_summary_II.sql

--- sql/1587_bank_account_summary_II.sql
+++ sql/1587_bank_account_summary_II.sql
@@ -1,4 +1,4 @@
 SELECT Users.name AS name, SUM(Transactions.amount) AS balance
-FROM Users LEFT JOIN Transactions 
+FROM Users JOIN Transactions 
 ON Users.account=Transactions.account
 GROUP BY name HAVING balance>10000;
