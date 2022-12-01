b470a976b8dc99453e9f0c42fb9ae741c413f66a
Fatma Kahveci <fatmaba@gmail.com>
2022-12-01 10:18:29 +0000

    Update 350_intersection_of_two_arrays_II.py

--- python/350_intersection_of_two_arrays_II.py
+++ python/350_intersection_of_two_arrays_II.py
@@ -1,9 +1,15 @@
 class Solution:
-    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        ret = []
-        checker = set(nums1)
-        for i in nums2:
-            if i in checker:
-                ret.append(i)
-                checker.remove(i)
-        return ret
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+
+        num1_counter_dict = dict()
+        common_nums_list = []
+        
+        for num1 in nums1:
+            num1_counter_dict[num1] = 1 if num1 not in num1_counter_dict else num1_counter_dict[num1] + 1
+                
+        for num2 in nums2:
+            if num2 in num1_counter_dict and num1_counter_dict[num2] != 0:
+                common_nums_list.append(num2)
+                num1_counter_dict[num2] -= 1
+
+        return common_nums_list
