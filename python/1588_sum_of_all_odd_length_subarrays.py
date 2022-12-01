b3cb603b515c48cfa6d4e10c05b1451fa0a341a1
Fatma Kahveci <fatmaba@gmail.com>
2022-12-01 14:29:02 +0000

    Update 1588_sum_of_all_odd_length_subarrays.py

--- python/1588_sum_of_all_odd_length_subarrays.py
+++ python/1588_sum_of_all_odd_length_subarrays.py
@@ -1,7 +1,7 @@
 class Solution:
     def sumOddLengthSubarrays(self, arr: List[int]) -> int:
         res = 0
-        for l in range(1, len(arr)+1, 2):
-            for r in range(len(arr)-l+1):
-                res += sum(arr[r:l+r])
+        for r in range(1, len(arr)+1, 2):
+            for l in range(len(arr)-r+1):
+                res += sum(arr[l:l+r])
         return res
