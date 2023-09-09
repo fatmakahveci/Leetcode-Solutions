f75b537c9aedb1c956e3fedaef105aa18cfb4901
Fatma Kahveci <fatmaba@gmail.com>
2023-09-09 12:08:06 +0300

    Fix 2695

--- TypeScript/2695_array_wrapper.ts
+++ TypeScript/2695_array_wrapper.ts
@@ -1,23 +1,15 @@
 class ArrayWrapper {
-    private nums: number[];
+    private nums: number
 
-	constructor(nums: number[]) {
+    constructor(nums: number[]) {
         this.nums = nums;
     }
 
-	valueOf() {
+    valueOf() {
         return this.nums.reduce((sum,val) => sum + val, 0);
     }
 
-	toString() {
+    toString() {
         return JSON.stringify(this.nums);
     }
 };
-
-/**
- * const obj1 = new ArrayWrapper([1,2]);
- * const obj2 = new ArrayWrapper([3,4]);
- * obj1 + obj2; // 10
- * String(obj1); // "[1,2]"
- * String(obj2); // "[3,4]"
- */
